452e461314f334c8d163b087060b84ecf44ff04c
BuiHenry1404 <buihenry1404@gmail.com>
2025-09-18 16:38:22 +0700

    config cors and create url util public

---
+++ src/main/java/com/henry/book_store/utils/UrlUtil.java
@@ -0,0 +1,18 @@
+package com.henry.book_store.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class UrlUtil {
+
+    public static final String API_VERSION = "/v1";
+    public static final String PREFIX = "/api";
+    public static final String BASE_URL = PREFIX + API_VERSION;
+
+    public static final String CATEGORY_URL = BASE_URL + "/categories";
+
+
+    public static final List<String> PUBLIC_URLS = Arrays.asList(
+            CATEGORY_URL
+    );
+}
